3599a5ffeb14be1d92609d91d77319cb31de8286
Al Hasan <alhasan375935@gmail.com>
2024-02-20 16:35:03 +0600

    family phone number added

---
+++ scripts/index.js
@@ -0,0 +1,48 @@
+
+function firstClick(){
+    buttonActiveColor('first-Section');
+}
+function nameClick(){
+    buttonActiveColor('baba')
+    
+}
+
+function secondClick(){
+    buttonActiveColor('ma')
+}
+function thirdClick(){
+    buttonActiveColor('bhai')
+}
+
+function fourClick(){
+    buttonActiveColor('tamanna')
+}
+
+function fiveClick(){
+    buttonActiveColor('hasan');
+    
+}
+
+// second button
+
+function optionClick(){
+    buttonActiveColor('second-Section')
+}
+
+
+function namesClick(){
+    buttonActiveColor('bab')
+}
+
+function secondsClick(){
+    buttonActiveColor('mas')
+}
+function thirdsClick(){
+    buttonActiveColor('bhas')
+}
+function foursClick(){
+    buttonActiveColor('tamannas')
+}
+function fivesClick(){
+    buttonActiveColor('hasans')
+}
---
+++ scripts/second.js
@@ -0,0 +1,38 @@
+function buttonActiveColor(elementId){
+    const buttonColor = document.getElementById(elementId);
+    const changeClick = buttonColor.classList.remove('hidden');
+    console.log(changeClick);
+}
+// family
+document.getElementById('input-data').addEventListener('keyup',function(event){
+    const text = event.target.value;
+    const disabledButton = document.getElementById('firstButton');
+
+    if(text === 'Family1'){
+        disabledButton.removeAttribute('disabled');
+        disabledButton.style.backgroundColor = 'green';
+    }
+    else{
+        disabledButton.setAttribute('disabled', true);
+        disabledButton.style.backgroundColor = 'yellow';
+        alert.call
+    }
+
+})
+// university
+
+document.getElementById('input-data').addEventListener('keyup',function(event){
+    const text = event.target.value;
+    const disabledButton = document.getElementById('secondButton');
+
+    if(text === 'University1'){
+        disabledButton.removeAttribute('disabled');
+        disabledButton.style.backgroundColor = 'green';
+    }
+    else{
+        disabledButton.setAttribute('disabled', true);
+        disabledButton.style.backgroundColor = 'yellow';
+        
+    }
+
+})
